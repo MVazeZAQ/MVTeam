6414720c60bfefb69c4694d168ae553d1613d805
MVazeZAQ <a.vahabov@inbox.ru>
2024-10-30 14:52:40 +0100

    created - views for notes model

--- inotes/inotesapp/views.py
+++ inotes/inotesapp/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+from inotesapp.models import Note
+from inotesapp.serializers import NoteSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+
+@api_view(['GET','POST'])
+def notes(request):
+    if request.method == 'GET':
+        notes = Note.objects.all()
+        serizalizer = NoteSerializer(notes, many = True)
+        return Response(serizalizer.data)
+    elif request.method == 'POST':
+        serizalizer = NoteSerializer(data=request.data)
+        if serizalizer.is_valid():
+            serizalizer.save()
+            return Response(serizalizer.data,status=status.HTTP_201_CREATED)
+        return Response(serizalizer.errors,status=status.HTTP_400_BAD_REQUEST)
