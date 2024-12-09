33934976f0483b96268c245a7e8e693a5abea3c7
MVazeZAQ <a.vahabov@inbox.ru>
2024-12-09 18:00:45 +0100

    delete exisiting Navbar from Homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,16 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
-// import MainLayout from "./layouts/MainLayout";
+import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
 
 const App = () => {
   const router = createBrowserRouter(
-    createRoutesFromElements(<Route index element={<HomePage />} />)
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+      </Route>
+    )
   );
 
   return <RouterProvider router={router} />;
