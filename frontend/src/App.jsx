21d4d2fc581ba602bfd3ef2bb9fc18fef4790380
hajiyevmagsud <hajiyev.magsud.oxford@gmail.com>
2024-12-09 16:24:04 +0100

    display: in app page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import NavBar from "./components/Navbar";
+import Filter from "./components/Filter ";
 
 const App = () => {
   return (
     <>
       <NavBar />
+      <Filter />
     </>
   );
 };
