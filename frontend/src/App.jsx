5f1defbd41bed52cfc31a41a8df227675ef23018
selamsahabe <selam.sahabe.karadag@gmail.com>
2024-12-22 19:43:21 +0100

    Import new page on App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,8 @@ import {
 } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
-import AddNotes from "./components/AddNotes";
+import AddNotes from "./pages/AddNotes";
+import NotePage from "./pages/NotePage";
 
 const App = () => {
   const router = createBrowserRouter(
@@ -14,6 +15,7 @@ const App = () => {
       <Route path="/" element={<MainLayout />}>
         <Route index element={<HomePage />} />
         <Route path="/add-notes" element={<AddNotes />} />
+        <Route path="/notes-detail" element={<NotePage />} />
       </Route>
     )
   );
