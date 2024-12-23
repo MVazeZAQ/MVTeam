e47e809ccb71460e851712ce285ad3760d0e809e
OguzhanYazdicutan <bnfmjrl_aehit@students.vizja.pl>
2024-12-23 05:59:53 +0100

    show Loader based on loading state in prop

--- frontend/src/components/NoteCardContainer.jsx
+++ frontend/src/components/NoteCardContainer.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import NoteCard from "./NoteCard";
-
-const NoteCardContainer = ({ notes }) => {
+import Loader from "./Loader";
+const NoteCardContainer = ({ notes, loading }) => {
   return (
     <div className="container">
       <div className="note-has-grid row">
+        {loading && <Loader loading={loading} />}
         {notes.map((note) => (
           <NoteCard key={note.id} note={note} />
         ))}
