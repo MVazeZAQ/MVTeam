857a3dddc6d87f8efd42a0393582f7980e965808
VahidNuraddinov <nuradva_aehit@students.vizja.pl>
2024-12-23 05:36:14 +0100

    coloring notes based on category

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -5,13 +5,22 @@ import { Link } from "react-router-dom";
 
 const NoteCard = ({ note }) => {
   const body = `${note.body.split(" ").slice(0, 20).join(" ")} ...`;
+  const color =
+    note.category == "BUSINESS"
+      ? "blue"
+      : note.category == "PERSONAL"
+      ? "green"
+      : "purple";
 
   return (
     <div className="col-md-4 single-note-item all-category">
       <div className="card card-body">
-        <span className="side-stick"></span>
-        <FaNoteSticky style={{ marginLeft: "auto" }} />
-        <Link to={`/notes/${note.slug}`} style={{ textDecoration: "none" }}>
+        <span className="side-stick" style={{ backgroundColor: color }}></span>
+        <FaNoteSticky style={{ marginLeft: "auto", color: color }} />
+        <Link
+          to={`/notes/${note.slug}`}
+          style={{ textDecoration: "none", color: "black" }}
+        >
           <h5
             className="note-title text-truncate w-75 mb-0"
             data-noteheading="Book a Ticket for Movie"
@@ -32,7 +41,9 @@ const NoteCard = ({ note }) => {
         <div className="d-flex align-items-center">
           <span className="d-flex justify-contents-around">
             <a href="/notes-detail">
-              <MdMarkunread style={{ fontSize: "25px", cursor: "pointer" }} />
+              <MdMarkunread
+                style={{ fontSize: "25px", cursor: "pointer", color: color }}
+              />
             </a>
 
             <small className="text-muted">{note.category}</small>
