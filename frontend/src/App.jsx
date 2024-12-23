a415a7fdfe31570b8fded55580afef5243dfa025
Sevval-Ozyurt <sevvalozyurt8@gmail.com>
2024-12-23 04:13:00 +0100

    Createing hardcode version of editing a note version

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
 import AddNotes from "./pages/AddNotes";
 import NotePage from "./pages/NotePage";
+import EditNotes from "./pages/EditNotePage";
 
 const App = () => {
   const router = createBrowserRouter(
@@ -16,6 +17,7 @@ const App = () => {
         <Route index element={<HomePage />} />
         <Route path="/add-notes" element={<AddNotes />} />
         <Route path="/notes-detail" element={<NotePage />} />
+        <Route path="/edit-note" element={<EditNotes />} />
       </Route>
     )
   );
