44943b553e29b8b6d40fa8f5f322f4f76d500ab3
VahidNuraddinov <nuradva_aehit@students.vizja.pl>
2025-01-23 02:24:00 +0100

    update on homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -5,8 +5,13 @@ import NoteCardContainer from "../components/NoteCardContainer";
 const HomePage = ({ notes, loading, handleFilterText }) => {
   return (
     <>
-      <Filter handleFilterText={handleFilterText} />
-
+      {notes.length < 1 ? (
+        <h4 style={{ textAlign: "center", marginTop: "10px" }}>
+          There is no note found with the search phrase above
+        </h4>
+      ) : (
+        <Filter handleFilterText={handleFilterText} />
+      )}
       <NoteCardContainer notes={notes} loading={loading} />
     </>
   );
