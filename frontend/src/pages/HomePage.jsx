080f9fcfb312f1acadf3d1585769ef5619104988
VahidNuraddinov <nuradva_aehit@students.vizja.pl>
2025-01-23 02:23:39 +0100

    filtering notes based on type

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Filter from "../components/Filter";
 import NoteCardContainer from "../components/NoteCardContainer";
 
-const HomePage = ({ notes, loading }) => {
+const HomePage = ({ notes, loading, handleFilterText }) => {
   return (
     <>
+      <Filter handleFilterText={handleFilterText} />
+
       <NoteCardContainer notes={notes} loading={loading} />
     </>
   );
