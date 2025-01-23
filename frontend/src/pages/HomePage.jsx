ba9953ee713fa773accb5adc6320c1c48183315d
VahidNuraddinov <nuradva_aehit@students.vizja.pl>
2025-01-23 02:12:07 +0100

    my commit

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import Filter from "../components/Filter";
 import NoteCardContainer from "../components/NoteCardContainer";
 
 const HomePage = ({ notes, loading }) => {
   return (
     <>
-      <Filter />
       <NoteCardContainer notes={notes} loading={loading} />
     </>
   );
